eb0ae87a730ed039cb3cd60bc8becce4518005fe
berylcp <berylcp@gmail.com>
2021-09-04 18:09:46 +0700

    4 files modified, 1 added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # belajar-react-redux
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/belajar-react-redux)
+
+[Extensions used] (https://github.com/zalmoxisus/redux-devtools-extension)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
+import './style.css';
 
 export default function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <h1>Counter {counter}</h1>
+
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLogged ? <h3>Valuable information i shouldn't see</h3> : ''}
     </div>
   );
 }
