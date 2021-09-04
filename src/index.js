06188f6cbc1b0b85ccc27372ec756077d980ec5f
berylcp <berylcp@gmail.com>
2021-09-04 17:20:46 +0700

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createStore } from 'redux';
 
-import App from "./App";
+// Store -> Globalized State
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Action Increment
+const increment = () => {
+  return {
+    tyoe: 'INCREMENT'
+  };
+};
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  };
+};
+
+// Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// Display in the console
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch
+store.dispatch(increment());
+
+ReactDOM.render(<App />, document.getElementById('root'));
