393dbca995d7d3b83bc76f2ef1f3972ab4701b29
berylcp <berylcp@gmail.com>
2021-09-04 17:41:43 +0700

    1 file modified, 3 added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import allReducer from './reducers';
+
+const store = createStore(allReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
